84b0750004a8f58981035a4533903bcaefa4eb64
Devam Darji <devamdarji22@gmail.com>
2021-01-08 20:17:58 +0530

    Latest database added

--- quizdatabase.sql
+++ quizdatabase.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 01, 2021 at 12:08 PM
+-- Generation Time: Jan 07, 2021 at 05:58 PM
 -- Server version: 10.4.11-MariaDB
 -- PHP Version: 7.4.3
 
@@ -22,6 +22,29 @@ SET time_zone = "+00:00";
 -- Database: `quizdatabase`
 --
 
+DELIMITER $$
+--
+-- Procedures
+--
+CREATE DEFINER=`root`@`localhost` PROCEDURE `test_marks_addition` (IN `testcode` VARCHAR(15), IN `mail` VARCHAR(50))  UPDATE attendance_details
+SET attendance_details.test_marks = (
+SELECT mark 
+FROM 
+
+(SELECT SUM(q.question_marks) AS mark, a.user_email as user_email , q.code as testcode
+FROM question_detail AS q
+INNER JOIN answer_detail AS a 
+    ON testcode = a.code AND q.code = a.code
+WHERE q.question_type = 'mcq' AND q.correct_answer = a.answer
+ AND a.question_number = q.question_number
+GROUP BY a.user_email) table1
+
+WHERE user_email = mail
+)
+WHERE attendance_details.user_email = mail$$
+
+DELIMITER ;
+
 -- --------------------------------------------------------
 
 --
@@ -35,6 +58,29 @@ CREATE TABLE `answer_detail` (
   `answer` varchar(1500) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `answer_detail`
+--
+
+INSERT INTO `answer_detail` (`code`, `user_email`, `question_number`, `answer`) VALUES
+('3J5HF0KMYF', 'd1@gmail.com', 1, '1'),
+('3J5HF0KMYF', 'd1@gmail.com', 2, '42'),
+('3J5HF0KMYF', 'd1@gmail.com', 3, '4'),
+('3J5HF0KMYF', 'd2@gmail.com', 1, '1'),
+('3J5HF0KMYF', 'd2@gmail.com', 2, 'Answer is written in pdf.'),
+('3J5HF0KMYF', 'd2@gmail.com', 3, '4'),
+('3J5HF0KMYF', 'd@gmail.com', 1, '1'),
+('3J5HF0KMYF', 'd@gmail.com', 2, '23'),
+('3J5HF0KMYF', 'd@gmail.com', 3, '1');
+
+--
+-- Triggers `answer_detail`
+--
+DELIMITER $$
+CREATE TRIGGER `test_marks_addition_trigger` AFTER INSERT ON `answer_detail` FOR EACH ROW CALL `test_marks_addition`(NEW.code,NEW.user_email)
+$$
+DELIMITER ;
+
 -- --------------------------------------------------------
 
 --
@@ -45,8 +91,26 @@ CREATE TABLE `attendance_details` (
   `test_code` varchar(15) NOT NULL,
   `user_email` varchar(50) NOT NULL,
   `attendance` tinyint(1) NOT NULL,
-  `test_marks` int(11) NOT NULL
+  `test_marks` int(11) NOT NULL,
+  `test_document_name` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `attendance_details`
+--
+
+INSERT INTO `attendance_details` (`test_code`, `user_email`, `attendance`, `test_marks`, `test_document_name`) VALUES
+('3J5HF0KMYF', 'd1@gmail.com', 1, 20, ''),
+('3J5HF0KMYF', 'd2@gmail.com', 1, 20, '0701211610010168unix 1bg18cs027 assignment.pdf'),
+('3J5HF0KMYF', 'd@gmail.com', 1, 10, '');
+
+--
+-- Triggers `attendance_details`
+--
+DELIMITER $$
+CREATE TRIGGER `remove_answer` AFTER DELETE ON `attendance_details` FOR EACH ROW DELETE FROM answer_detail WHERE user_email = OLD.user_email
+$$
+DELIMITER ;
 
 -- --------------------------------------------------------
 
@@ -65,10 +129,8 @@ CREATE TABLE `host` (
 --
 
 INSERT INTO `host` (`Email`, `Name`, `Password`) VALUES
-('abc@gmail.com', 'abc', '1234'),
 ('atharva@gmail.com', 'Atharva', '1234'),
 ('chinmay@gmail.com', 'chinmay', '1234'),
-('d@gmail.com', 'Devam darji', '1234'),
 ('devamdarji22@gmail.com', 'Devam darji', '1234'),
 ('jay@gmail.com', 'jay', '1234');
 
@@ -81,7 +143,6 @@ INSERT INTO `host` (`Email`, `Name`, `Password`) VALUES
 CREATE TABLE `question_detail` (
   `code` varchar(15) NOT NULL,
   `question_number` int(5) NOT NULL,
-  `question_part` varchar(2) NOT NULL,
   `question_type` varchar(15) NOT NULL,
   `question` varchar(500) NOT NULL,
   `question_marks` int(11) NOT NULL,
@@ -92,6 +153,15 @@ CREATE TABLE `question_detail` (
   `correct_answer` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `question_detail`
+--
+
+INSERT INTO `question_detail` (`code`, `question_number`, `question_type`, `question`, `question_marks`, `option1`, `option2`, `option3`, `option4`, `correct_answer`) VALUES
+('3J5HF0KMYF', 1, 'mcq', 'What is your name ?', 10, 'Devam', 'Vaidehi', 'Prakash', 'Kalpana', 1),
+('3J5HF0KMYF', 2, 'subjective', 'What is your age ?', 5, '', '', '', '', 0),
+('3J5HF0KMYF', 3, 'mcq', 'Which of these is not an animal ?', 10, 'Vaidehi', 'Dog', 'Cat ', 'Devam', 4);
+
 -- --------------------------------------------------------
 
 --
@@ -100,12 +170,18 @@ CREATE TABLE `question_detail` (
 
 CREATE TABLE `test_detail` (
   `test_code` varchar(15) NOT NULL,
-  `test_password` varchar(20) NOT NULL,
   `test_host_email` varchar(50) NOT NULL,
   `test_title` varchar(50) NOT NULL,
   `start` tinyint(1) NOT NULL,
   `test_date` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `test_detail`
+--
+
+INSERT INTO `test_detail` (`test_code`, `test_host_email`, `test_title`, `start`, `test_date`) VALUES
+('3J5HF0KMYF', 'atharva@gmail.com', 'DBMS Internal 1', 1, '2021-01-06');
 
 -- --------------------------------------------------------
 
